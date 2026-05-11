61f27b82953ffb75f81ada014fea87548e94eeb7
Sergii Bomko <1164492+aquiladev@users.noreply.github.com>
2026-05-11 12:24:19 +0300

    feat(receipts): controller + service + repository + DTOs + tests

---
+++ libs/modules/receipts/src/index.ts
@@ -0,0 +1,4 @@
+export { ReceiptsModule } from './receipts.module.js';
+export { ReceiptsService } from './receipts.service.js';
+export { ReceiptsRepository } from './receipts.repository.js';
+export { ReceiptsController } from './receipts.controller.js';
